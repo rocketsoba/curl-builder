04b66c4d18a736793995278685925b2b1b0461eb
rockyakisoba <mmtrokuroku96@gmail.com>
2017-10-07 20:09:01 +0900

    initial commit

---
+++ examples/ConnectGoogle.php
@@ -0,0 +1,14 @@
+<?php
+
+date_default_timezone_set("Asia/Tokyo");
+ini_set("arg_separator.output", "&");
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
+ini_set('xdebug.var_display_max_depth', -1);
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Curl\MyCurl;
+
+$lib1 = new MyCurl("https://www.google.com/");
+echo $lib1->getReshead();
