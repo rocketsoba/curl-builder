2023a2f7133263a6bcd07f947d3304315a4a54e8
rockyakisoba <mmtrokuroku96@gmail.com>
2019-06-04 20:32:59 +0900

    例外の名前空間にtypoがあったので修正

--- src/Exception/CFBypassFailedException.php
+++ src/Exception/CFBypassFailedException.php
@@ -1,11 +1,21 @@
 <?php
 
-namespace \Curl\Exception;
+namespace Curl\Exception;
 
 use \Exception;
 
+/**
+ * Cloudflareのバイパスに失敗したときの例外
+ *
+ * @todo @expectedExceptionMessageというアノテーションがPHPUnitにあるのでMessageさえ渡せば必要ない？
+ */
 class CFBypassFailedException extends Exception
 {
+    /**
+     * CFBypassFailedExceptionのコンストラクタ
+     *
+     * @param string $message
+     */
     public function __construct($message = "")
     {
         parent::__construct("Bypassing Cloudflare was failed.");
